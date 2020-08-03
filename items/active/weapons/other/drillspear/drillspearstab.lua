2154d1b82d9551d76fee4ff48b5be11945d2e995
Donmor <denner.urbano@gmail.com>
2020-08-03 09:29:50 -0300

    Initial commit

---
+++ items/active/weapons/other/drillspear/drillspearstab.lua
@@ -0,0 +1,29 @@
+require "/items/active/weapons/melee/meleeslash.lua"
+
+-- Spear stab attack
+-- Extends normal melee attack and adds a hold state
+DrillSpearStab = MeleeSlash:new()
+
+function DrillSpearStab:init()
+  MeleeSlash.init(self)
+
+  self.holdDamageConfig = sb.jsonMerge(self.damageConfig, self.holdDamageConfig)
+  self.holdDamageConfig.baseDamage = self.holdDamageMultiplier * self.damageConfig.baseDamage
+end
+
+function DrillSpearStab:hold()
+  self.weapon:setStance(self.stances.hold)
+  self.weapon:updateAim()
+
+  animator.setAnimationState("drill", "active1")
+
+  while self.fireMode == "primary" do
+    local damageArea = partDamageArea("spear")
+    self.weapon:setDamage(self.holdDamageConfig, damageArea)
+    coroutine.yield()
+  end
+
+  animator.setAnimationState("drill", "idle")
+
+  self.cooldownTimer = self:cooldownTime()
+end
